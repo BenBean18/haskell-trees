6e21a5fef8fc77c05559b893ad6c0b5b4508fe10
BenBean18 <beakerthespeaker@gmail.com>
2023-09-15 13:39:45 -0400

    make generic and have inOrder -> [...]

--- app/BST.hs
+++ app/BST.hs
@@ -3,29 +3,27 @@ module BST where
 import Data.Maybe
 
 -- Type for a binary search tree node -- contains data and potentially a left and right child
-data Node = Node { i :: Int, left :: Maybe Node, right :: Maybe Node } deriving (Ord, Show, Eq)
+data Node t = Node { i :: t, left :: Maybe (Node t), right :: Maybe (Node t) } deriving (Ord, Show, Eq)
 
 -- Start at the root.
 -- If the node to be added's data <= the current root node's data, move to the left. If there is no left child, add the new node there.
 -- If the node to be added's data > the current root node's data, move to the right. If there is no right child, add the new node there.
 
 -- insert root newNode -> newTree
-insert :: Node -> Node -> Node
-insert root node = 
-    if (i node) <= (i root) then
-        if isNothing (left root) then Node { i = i root, left = Just node, right = right root }
+insert :: Ord t => Node t -> Node t -> Node t
+insert root node
+  | (i node) <= (i root) = if isNothing (left root) then Node { i = i root, left = Just node, right = right root }
         else Node { i = i root, left = Just (insert (fromJust $ left root) node), right = right root }
-    else
-        if isNothing (right root) then Node { i = i root, left = left root, right = Just node }
-        else Node { i = i root, left = left root, right = Just (insert (fromJust $ right root) node) }
+  | isNothing (right root) = Node { i = i root, left = left root, right = Just node }
+  | otherwise = Node { i = i root, left = left root, right = Just (insert (fromJust $ right root) node) }
 
 -- in-order traversal: interact with parent in the middle of children
-inOrder :: Node -> IO ()
-inOrder Node { i = i, left = Nothing, right = Nothing } = print i
-inOrder node = do
-    if isJust $ left node then inOrder $ (fromJust (left node)) else return ()
-    print (i node)
-    if isJust $ right node then inOrder $ (fromJust (right node)) else return ()
+inOrder :: Node t -> [t]
+inOrder Node { i = i, left = Nothing, right = Nothing } = [i]
+inOrder node =
+    (if isJust $ left node then inOrder $ fromJust (left node) else []) ++
+    [i node] ++
+    (if isJust $ right node then inOrder $ fromJust (right node) else [])
 
-node :: Int -> Node
+node :: i -> Node i
 node i = Node { i = i, left = Nothing, right = Nothing }
--- app/Main.hs
+++ app/Main.hs
@@ -5,5 +5,5 @@ import Data.Maybe
 
 main :: IO ()
 main =
-    let tree = insert (insert (insert (insert (node 18) (node 36)) (node 30)) (node 1)) (node 1000) in do
-        inOrder tree
+    let tree = insert (insert (insert (insert (node 18) (node 36)) (node 30)) (node 1)) (node 1000) in 
+        print $ inOrder tree
